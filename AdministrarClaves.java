a083b8bb76dfb3391af9d9b7e0e381c0d1c47fba
ronaldo23152 <149660052+ronaldo23152@users.noreply.github.com>
2023-12-07 08:21:19 -0500

    AdministrarClaves.java

--- AdministrarClaves.java
+++ AdministrarClaves.java
@@ -1,16 +1,23 @@
-package DAO;
-import Format.*;
+package Formato;
+import DAO.ConectarBD;
 
-public class AdministrarClaves extends ConexiónDB {
+
+public class AdministrarClaves extends ConectarBD {
     public AdministrarClaves(){}
     
-    public String consultacat=" select idcategoria from categorias where nombre=?;";
-    public String consultamat=" select idmaterial from materiales where nombre=?;";
-    public String consultacol=" select idcolor from colores where nombre=?;";
+    public String cidcategoria=" select idcategoria from categorias where nombre=?;";
+    public String cidmaterial=" select idmaterial from materiales  where nombre=?;";
+    public String cidcolor=" select idcolor from colores  where nombre=?;";
+    public String cidproducto=" select idproducto from productos  where nombre=?;";
+    public String cidcliente=" select idcliente from clientes  where nombre=?;";
+    public String cidempleado=" select idempleado from empleados where nombre=?;";
     
-    public String consulta1="select nombre from categorias where idcategoria=?";
-    public String consulta2="select nombre from proveedores where idproveedor=?";
-    public String consulta3="select nombre from productos where idproducto=?";
+    public String cnombrecategoria="select nombre from categorias where idcategoria=?";
+    public String cnombrematerial="select nombre from materiales where idmaterial=?";
+    public String cnombrecolor="select nombre from colores where idcolor=?";
+    public String cnombreproducto="select nombre from productos where idproducto=?";
+    public String cnombrecliente="select nombre from clientes where idcliente=?";
+    public String cnombreempleado="select nombre from empleados where idempleado=?";
 
     public String RecuperarNombre(String consulta,int parametro){
         String nombre="";
@@ -23,7 +30,7 @@ public class AdministrarClaves extends ConexiónDB {
             }
             conexion.close();
         }catch(Exception ex){
-            Mensajes1.M1("ERROR no se pueden recuperar los nombres..."+ex);
+            Mensajes.M1("ERROR no se pueden recuperar los nombres..."+ex);
         }
         return nombre;
     }
@@ -37,7 +44,7 @@ public class AdministrarClaves extends ConexiónDB {
                 id = rs.getInt(1);
             }    
         }catch(Exception ex){
-            Mensajes1.M1("ERROR no se puede recuperar el ID..."+ex);
+            Mensajes.M1("ERROR no se puede recuperar el ID..."+ex);
         }
         return id;
     }
