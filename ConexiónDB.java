46955d5df52bdef0fcf3c6b7123351007702c12a
ronaldo23152 <149660052+ronaldo23152@users.noreply.github.com>
2023-12-07 08:21:57 -0500

    ConectarBD.java

--- ConexiónDB.java
+++ ConexiónDB.java
@@ -1,27 +1,21 @@
-
 package DAO;
-import Format.*;
+//librerias
 import java.sql.*;
+import Formato.*;
 
-
-public class ConexiónDB implements parámetros {
+public class ConectarBD implements Parametros{
     public Connection conexion;
     public Statement st;
     public ResultSet rs;
     public PreparedStatement ps;
-    public ResultSetMetaData mdata;    
-
-    public ConexiónDB() {
-        try {       
+    public ResultSetMetaData mdata;
+    public ConectarBD(){
+        try {
             Class.forName(DRIVER);
             conexion = DriverManager.getConnection(RUTA, USUARIO, CLAVE);
             st = conexion.createStatement();
-          
         } catch (Exception ex) {
-            Mensajes1.M1("Something went wrong... -_-" + ex);
+            Mensajes.M1("ERROR, NO SE PUDO CONECTAR A LA BASE DE DATOS" + ex);
         }
-       
-    }   
-
- 
+    }
 }
