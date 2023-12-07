d04252a5991b8b44ae68c460f68652f9ced42997
ronaldo23152 <149660052+ronaldo23152@users.noreply.github.com>
2023-12-07 08:31:54 -0500

    Delete parámetros.java

--- parámetros.java
+++
@@ -1,9 +0,0 @@
-
-package DAO;
-
-public interface parámetros {
-    String DRIVER = "com.mysql.cj.jdbc.Driver";
-    String RUTA = "jdbc:mysql://localhost:3306/HELLEN1512";
-    String USUARIO = "root";
-    String CLAVE = "axcc";
-}
