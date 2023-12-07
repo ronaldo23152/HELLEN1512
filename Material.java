4ba3acc67ee8c83518d62d8c2c7d29c122a87aef
ronaldo23152 <149660052+ronaldo23152@users.noreply.github.com>
2023-12-07 08:24:09 -0500

    Update Material.java

--- Material.java
+++ Material.java
@@ -1,22 +1,52 @@
 package Modelo;
-//librerias
-import lombok.Getter;
-import lombok.Setter;
-//getter and setter
-@Getter
-@Setter
-//clase
-public class Material {    
+
+public class Material extends Producto{    
 //atributos
     private int idmaterial;
-    private String nombrematerial;    
-    private String indicador;
+    private String nombre;    
+    private String descripcion;
+    private String indicador;    
 //constructor
     public Material(){}
+
+    public int getIdmaterial() {
+        return idmaterial;
+    }
+
+    public void setIdmaterial(int idmaterial) {
+        this.idmaterial = idmaterial;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getIndicador() {
+        return indicador;
+    }
+
+    public void setIndicador(String indicador) {
+        this.indicador = indicador;
+    }
+    
+    
 //array
-    public Object[] RegistroMaterial(int numeracion){
-        Object[] fila = {numeracion,idmaterial,nombrematerial};
+    public Object[] RegistroMaterial(int num){
+        Object[] fila = {num,idmaterial,nombre,descripcion};
         return fila;
     }
+
 }
 //fin class
