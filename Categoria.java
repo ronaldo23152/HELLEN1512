c4ce8a50b9aa836f0473ab5a1cd7d51d98ed1d1e
ronaldo23152 <149660052+ronaldo23152@users.noreply.github.com>
2023-12-07 08:20:54 -0500

    Categoria.java

--- Categoria.java
+++ Categoria.java
@@ -1,22 +1,53 @@
 package Modelo;
-//librerias
-import lombok.Getter;
-import lombok.Setter;
-//getter and setter
-@Getter
-@Setter
+
 //clase
 public class Categoria {    
+    
 //atributos
     private int idcategoria;
-    private String nombrecategoria;
+    private String nombre;
+    private String descripcion;
     private String indicador;
 //constructor
     public Categoria(){}    
+
+    public int getIdcategoria() {
+        return idcategoria;
+    }
+
+    public void setIdcategoria(int idcategoria) {
+        this.idcategoria = idcategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getIndicador() {
+        return indicador;
+    }
+
+    public void setIndicador(String indicador) {
+        this.indicador = indicador;
+    }   
+    
+    
 //array
-    public Object[] RegistroCategoria(int num) {
-        Object[] fila = {num,idcategoria,nombrecategoria};
+    public Object[] RegistroCategoria(int num) {        
+        Object[] fila = {num,idcategoria,nombre,descripcion};
         return fila;
-    }  
-}
-//fin class
+    }    
+
+}//fin class
